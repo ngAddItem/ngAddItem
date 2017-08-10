7246cb238aea648fac4658c44f53115c627f9b0b
doug bradley <dougb@erequester.com>
2017-08-10 09:48:14 -0700

    pre-routing checkin

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
 import { RequisitionService } from './model/requisition.service';
-
+import { FieldsetModule } from 'primeng/primeng';
 import { AppComponent } from './app.component';
 import { RequisitionComponent } from './requisition/requisition.component';
 import { DataTableModule } from 'primeng/primeng';
 import { RequisitionDetailComponent } from './requisition-detail/requisition-detail.component';
+import { BrowserAnimationsModule  } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -18,6 +20,9 @@ import { RequisitionDetailComponent } from './requisition-detail/requisition-det
     BrowserModule,
     HttpModule,
     DataTableModule,
+    FieldsetModule,
+    BrowserAnimationsModule,
+    ReactiveFormsModule,
   ],
   providers: [RequisitionService],
   bootstrap: [AppComponent]
--- src/app/requisition-detail/requisition-detail.component.ts
+++ src/app/requisition-detail/requisition-detail.component.ts
@@ -1,16 +1,29 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, OnChanges, Input, SimpleChanges } from '@angular/core';
 import { Requisition } from '../model/requisition.model';
+import { FormBuilder, FormGroup, FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-requisition-detail',
   templateUrl: './requisition-detail.component.html',
   styleUrls: ['./requisition-detail.component.css']
 })
-export class RequisitionDetailComponent implements OnInit {
+export class RequisitionDetailComponent implements OnInit, OnChanges {
+
   @Input() requisition: Requisition;
+  myForm: FormGroup;
 
-  constructor() { }
-
+  constructor(private fb: FormBuilder) {
+    this.myForm = this.fb.group({
+      id: ['', Validators.required],
+      title: ['', Validators.required]
+    })
+   }
+  ngOnChanges(changes: SimpleChanges): void {
+    this.myForm.setValue({
+      id: this.requisition.requisitionId,
+      title: this.requisition.requisitionTitle
+    });
+  }
   ngOnInit() {
   }
 
